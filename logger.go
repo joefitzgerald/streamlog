6e144b2a808045fb88f72f746363c01c2d3706a8
Joe Fitzgerald <jfitzgerald@gopivotal.com>
2014-08-09 21:43:30 -0600

    Implementing Interface Functions - Don't export outErrLogger - New... funcs return StreamLogger, not implementation

--- logger.go
+++ logger.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joefitzgerald/standardlog"
 )
@@ -34,34 +35,135 @@ type StreamLogger interface {
 	SetPrefix(prefix string)
 }
 
+// New creates a new StreamLogger that writes to os.Stdout and os.Stderr.
+func New() StreamLogger {
+	return NewWithWriters(os.Stdout, os.Stderr, "", log.LstdFlags)
+}
+
 // NewWithWriters creates a new StdOutErrStreamLogger. The out and err variables set the
 // destination to which log data will be written.
 // The prefix appears at the beginning of each generated log line.
 // The flag argument defines the logging properties.
-func NewWithWriters(out io.Writer, err io.Writer, prefix string, flag int) *StdOutErrStreamLogger {
+func NewWithWriters(out io.Writer, err io.Writer, prefix string, flag int) StreamLogger {
 	outLogger := log.New(os.Stdout, prefix, log.LstdFlags)
 	errLogger := log.New(os.Stderr, prefix, log.LstdFlags)
-	return &StdOutErrStreamLogger{out: outLogger, err: errLogger, prefix: prefix, flag: flag}
+	return &outErrStreamLogger{out: outLogger, err: errLogger, prefix: prefix, flag: flag}
 }
 
 // NewWithLoggers creates a new StdOutErrStreamLogger. The out and err variables
 // set the destination to which log data will be written.
 // The prefix appears at the beginning of each generated log line.
 // The flag argument defines the logging properties.
-func NewWithLoggers(out standardlog.Logger, err standardlog.Logger, prefix string, flag int) *StdOutErrStreamLogger {
-	return &StdOutErrStreamLogger{out: out, err: err, prefix: prefix, flag: flag}
+func NewWithLoggers(out standardlog.Logger, err standardlog.Logger, prefix string, flag int) StreamLogger {
+	return &outErrStreamLogger{out: out, err: err, prefix: prefix, flag: flag}
 }
 
 // StdOutErrStreamLogger is an OutErrLogger using stderr and stdout
-type StdOutErrStreamLogger struct {
+type outErrStreamLogger struct {
+	mu     sync.Mutex         // ensures atomic writes; protects the following fields
 	out    standardlog.Logger // Logger for OutputStream.Out
 	err    standardlog.Logger // Logger for OutputStream.Err
 	prefix string             // prefix to write at beginning of each line
 	flag   int                // properties
 }
 
-// New creates a new StdOutErrStreamLogger that writes to os.Stdout and
-// os.Stderr.
-func (l *StdOutErrStreamLogger) New() *StdOutErrStreamLogger {
-	return NewWithWriters(os.Stdout, os.Stderr, "", log.LstdFlags)
+// Flags returns the output flags for the logger.
+func (l *outErrStreamLogger) Flags() int {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.flag
+}
+
+// Prefix returns the output prefix for the logger.
+func (l *outErrStreamLogger) Prefix() string {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.prefix
+}
+
+// SetPrefix sets the output prefix for the logger.
+func (l *outErrStreamLogger) SetPrefix(prefix string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.prefix = prefix
+	l.out.SetPrefix(prefix)
+	l.err.SetPrefix(prefix)
+}
+
+// SetFlags sets the output flags for the logger.
+func (l *outErrStreamLogger) SetFlags(flag int) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.flag = flag
+	l.out.SetFlags(flag)
+	l.err.SetFlags(flag)
+}
+
+// Printf calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Printf.
+func (l *outErrStreamLogger) Printf(s OutputStream, format string, v ...interface{}) {
+	switch s {
+	case Out:
+		l.out.Printf(format, v)
+	case Err:
+		l.err.Printf(format, v)
+	}
+}
+
+// Print calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Print.
+func (l *outErrStreamLogger) Print(s OutputStream, v ...interface{}) {
+	switch s {
+	case Out:
+		l.out.Print(v)
+	case Err:
+		l.err.Print(v)
+	}
+}
+
+// Println calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Println.
+func (l *outErrStreamLogger) Println(s OutputStream, v ...interface{}) {
+	switch s {
+	case Out:
+		l.out.Println(v)
+	case Err:
+		l.err.Println(v)
+	}
+}
+
+// Printf calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Printf.
+func (l *outErrStreamLogger) PrintfErr(format string, v ...interface{}) {
+	l.err.Printf(format, v)
+}
+
+// Print calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Print.
+func (l *outErrStreamLogger) PrintErr(v ...interface{}) {
+	l.err.Print(v)
+}
+
+// Println calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Println.
+func (l *outErrStreamLogger) PrintlnErr(v ...interface{}) {
+	l.err.Println(v)
+}
+
+// Printf calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Printf.
+func (l *outErrStreamLogger) PrintfOut(format string, v ...interface{}) {
+	l.out.Printf(format, v)
+}
+
+// Print calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Print.
+func (l *outErrStreamLogger) PrintOut(v ...interface{}) {
+	l.out.Print(v)
+}
+
+// Println calls l.Output to print to the logger.
+// Arguments are handled in the manner of fmt.Println.
+func (l *outErrStreamLogger) PrintlnOut(v ...interface{}) {
+	l.out.Println(v)
 }
